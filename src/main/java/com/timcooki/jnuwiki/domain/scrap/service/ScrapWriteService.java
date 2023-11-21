5463b137ec1e1b111c092e69cf537f5eec23a0e3
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-21 14:47:26 +0900

    refactor: 문서, 회원 검증 후에 로직 처리

--- src/main/java/com/timcooki/jnuwiki/domain/scrap/service/ScrapWriteService.java
+++ src/main/java/com/timcooki/jnuwiki/domain/scrap/service/ScrapWriteService.java
@@ -1,13 +1,16 @@
 package com.timcooki.jnuwiki.domain.scrap.service;
 
+import com.timcooki.jnuwiki.domain.docs.entity.Docs;
+import com.timcooki.jnuwiki.domain.docs.exception.DocsNotFoundException;
+import com.timcooki.jnuwiki.domain.docs.repository.DocsRepository;
+import com.timcooki.jnuwiki.domain.member.entity.Member;
+import com.timcooki.jnuwiki.domain.member.exception.MemberNotFoundException;
+import com.timcooki.jnuwiki.domain.member.repository.MemberRepository;
 import com.timcooki.jnuwiki.domain.scrap.DTO.request.DeleteScrapReqDTO;
 import com.timcooki.jnuwiki.domain.scrap.DTO.request.NewScrapReqDTO;
 import com.timcooki.jnuwiki.domain.scrap.entity.Scrap;
 import com.timcooki.jnuwiki.domain.scrap.repository.ScrapRepository;
-import com.timcooki.jnuwiki.util.ApiResult;
-import com.timcooki.jnuwiki.util.ApiUtils;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,24 +19,28 @@ import org.springframework.transaction.annotation.Transactional;
 public class ScrapWriteService {
 
     private final ScrapRepository scrapRepository;
+    private final MemberRepository memberRepository;
+    private final DocsRepository docsRepository;
 
     @Transactional
-    public void create(NewScrapReqDTO newScrapReqDTO){
-        Scrap scrap = dtoToScrap(newScrapReqDTO.memberId(), newScrapReqDTO.docsId());
-        scrapRepository.save(scrap);
+    public void create(NewScrapReqDTO newScrapReqDTO) {
+        Member member = memberRepository.findByMemberId(newScrapReqDTO.memberId())
+                .orElseThrow(MemberNotFoundException::new);
+
+        Docs docs = docsRepository.findById(newScrapReqDTO.docsId())
+                .orElseThrow(DocsNotFoundException::new);
+
+        scrapRepository.save(Scrap.of(member, docs));
     }
 
     @Transactional
-    public void delete(DeleteScrapReqDTO deleteScrapReqDTO){
-        Scrap scrap = dtoToScrap(deleteScrapReqDTO.memberId(), deleteScrapReqDTO.docsId());
-        scrapRepository.delete(scrap);
-    }
+    public void delete(DeleteScrapReqDTO deleteScrapReqDTO) {
+        Member member = memberRepository.findByMemberId(deleteScrapReqDTO.memberId())
+                .orElseThrow(MemberNotFoundException::new);
 
-    private static Scrap dtoToScrap(Long memberId, Long docsId) {
-        return Scrap.builder()
-                .memberId(memberId)
-                .docsId(docsId)
-                .build();
-    }
+        Docs docs = docsRepository.findById(deleteScrapReqDTO.docsId())
+                .orElseThrow(DocsNotFoundException::new);
 
+        scrapRepository.delete(Scrap.of(member, docs));
+    }
 }
